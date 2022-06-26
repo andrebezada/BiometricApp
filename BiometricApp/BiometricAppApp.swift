3a111f0f385066553359fe1158da1d0d4aa0bd9a
Andre Fernando Bezada <a.bezada.maza@gmail.com>
2022-06-26 18:44:44 -0500

    Initial Commit

---
+++ BiometricApp/BiometricAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricAppApp.swift
+//  BiometricApp
+//
+//  Created by Andre Bezada on 26/06/22.
+//
+
+import SwiftUI
+
+@main
+struct BiometricAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
