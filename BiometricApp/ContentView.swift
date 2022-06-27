50795a7d93416fc867ba413c308d28bed0a195a9
Andre Fernando Bezada <a.bezada.maza@gmail.com>
2022-06-26 20:13:08 -0500

    native biometric component

--- BiometricApp/ContentView.swift
+++ BiometricApp/ContentView.swift
@@ -6,12 +6,57 @@
 //
 
 import SwiftUI
+import LocalAuthentication
+
 
 struct ContentView: View {
+    
+    @State private var auth = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text(auth ? "Estás autenticado":"Necesitas autenticarte")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+                .multilineTextAlignment(.center)
+            Button(auth ? "Cerrar" : "Auntenticar"){
+                
+                if auth {
+                    auth = false
+                } else {
+                    authentication()
+                }
+                                
+            }.buttonStyle(.borderedProminent)
+                .buttonBorderShape(.roundedRectangle)
+                .accentColor(.orange)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(auth ? Color.green : Color.red)
     }
+    
+    private func authentication(){
+        
+        var error: NSError?
+        
+        let laContext = LAContext()
+        if laContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics , error: &error){
+            
+            laContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Get authenticated using Biometric sensor"){ autenticated, error in
+                
+                if autenticated{
+                    auth = true
+                }
+                
+            }
+            
+        } else {
+            auth = true
+        }
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
